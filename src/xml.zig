84dc10795ddc45ec752c4d9d4e545cff94ce992a
InK <InKryption07@gmail.com>
2021-07-27 00:10:42 +0200

    Building blocks based off of past experience

--- src/xml.zig
+++ src/xml.zig
@@ -4,32 +4,47 @@ pub const Tokenizer = struct {
     buffer: []const u8,
     index: usize = 0,
     
-    
-    
     pub const Token = union(enum) {
+        markup: Markup,
+        element: Element,
         
-        
-        pub const Markup = union(enum) {
-            element: Element,
+        pub const Element = union(enum) {
+            begin_open: Index,        // `<`
+            begin_close: Index,       // `</`
+            end: Index,               // `>`
+            end_empty: Index,         // `/>`
             
-            pub const Element = union(enum) {
-                open_beg: Index,
-                open_end_parent: Index,
-                open_end_empty: Index,
-                
-                close_beg: Index,
-                close_end: Index,
-                
-                identifier: Range,
-                eql: Index,
-                string: Range,
-            };
+            identifier: Range,
+            eql: Index, // '='
+            
+            string_begin: Index, // effectively matches `"` or `'`.
+            string_end: Index, // effectively matches `"` or `'`.
+            entity_reference: Range, // first index matches '&', penultimate index matches ';'.
+        };
+        
+        pub const Index = struct {
+            index: usize,
+        };
+        
+        pub const Range = struct {
+            beg: usize,
+            end: usize,
+            
+            pub fn sliceFrom(self: Range, slice: []const u8) []const u8 {
+                return slice[self.beg..self.end];
+            }
+            
+            pub fn length(self: Range) usize {
+                std.debug.assert(self.end >= self.beg);
+                return self.end - self.beg;
+            }
             
         };
         
-        pub const Range = struct { beg: usize, end: usize };
-        pub const Index = struct { index: usize };
-        
+    };
+    
+    pub const ParseState = union(enum) {
+        start,
     };
     
 };
