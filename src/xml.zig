d056c165b5d75c5a3a1ab6ea04d155fb30fd577e
InKryption <InKryption07@gmail.com>
2021-08-11 04:27:03 +0200

    small progresses

--- src/xml.zig
+++ src/xml.zig
@@ -10,8 +10,6 @@ pub const Tokenizer = struct {
         bof,
         eof,
         
-        
-        
         element_begin: ElementOpen,
         element_close: ElementClose,
         text: Text,
@@ -24,7 +22,7 @@ pub const Tokenizer = struct {
             
             pub const Identifier = struct {
                 namespace: Range,
-                range: Range,
+                identifier: Range,
             };
             
             pub const Attribute = struct {
@@ -41,6 +39,7 @@ pub const Tokenizer = struct {
         pub const Text = union(enum) {
             plain: Range,
             char_data: Range,
+            empty_whitespace: Range,
         };
         
         pub const Comment = Range;
@@ -80,10 +79,20 @@ pub const Tokenizer = struct {
                     },
                 },
                 
+                .tag_pi_open
+                => unreachable,
+                
                 .tag_open
-                => |tag_open| switch (current_char) {
+                => switch (current_char) {
                     '?',
-                    => unreachable,
+                    => {
+                        self.parse_state = .tag_pi_open;
+                        self.index += 1;
+                        result = .{ .processing_instructions = .{ .target = .{
+                            .beg = self.index,
+                            .end = undefined,
+                        } } };
+                    },
                     
                     '!',
                     => unreachable,
@@ -97,6 +106,10 @@ pub const Tokenizer = struct {
                     else
                     => {
                         self.parse_state = .{ .tag_element_open = .check_valid_start_char };
+                        result = .{ .element_begin = .{ .identifier = .{
+                            .namespace = .{ .beg = self.index, .end = self.index },
+                            .identifier = .{ .beg = self.index, .end = undefined, },
+                        } } };
                     },
                     
                 },
@@ -104,6 +117,67 @@ pub const Tokenizer = struct {
                 .tag_element_open
                 => |*tag_element_open| switch (tag_element_open.*) {
                     .check_valid_start_char
+                    => {
+                        switch (current_char) {
+                            'A'...'Z',
+                            'a'...'z',
+                            '\u{C0}'...'\u{D6}',
+                            '\u{D8}'...'\u{F6}',
+                            '_',
+                            => {
+                                tag_element_open.* = .seek_element_name_end;
+                                self.index += 1;
+                            },
+                            
+                            else
+                            => {
+                                const utf8_byte_seq_len = std.unicode.utf8ByteSequenceLength(current_char) catch {
+                                    if (true) unreachable;
+                                    result = .{ .invalid = .{ .index = self.index } };
+                                    break;
+                                };
+                                
+                                if (self.index + utf8_byte_seq_len > self.buffer.len) {
+                                    if (true) unreachable;
+                                    result = .{ .invalid = .{ .index = self.index } };
+                                    break;
+                                }
+                                
+                                const utf8_cp = std.unicode.utf8Decode(self.buffer[self.index..self.index + utf8_byte_seq_len]) catch {
+                                    if (true) unreachable;
+                                    result = .{ .invalid = .{ .index = self.index } };
+                                    break;
+                                };
+                                
+                                switch (utf8_cp) {
+                                    '\u{F8}'...'\u{2FF}',
+                                    '\u{370}'...'\u{37D}',
+                                    '\u{37F}'...'\u{1FFF}',
+                                    '\u{200C}'...'\u{200D}',
+                                    '\u{2070}'...'\u{218F}',
+                                    '\u{2C00}'...'\u{2FEF}',
+                                    '\u{3001}'...'\u{D7FF}',
+                                    '\u{F900}'...'\u{FDCF}',
+                                    '\u{FDF0}'...'\u{FFFD}',
+                                    '\u{10000}'...'\u{EFFFF}',
+                                    => {
+                                        tag_element_open.* = .seek_element_name_end;
+                                        self.index += utf8_byte_seq_len;
+                                    },
+                                    
+                                    else
+                                    => {
+                                        if (true) unreachable;
+                                        result = .{ .invalid = .{ .index = self.index } };
+                                        break;
+                                    },
+                                }
+                                
+                            },
+                        }
+                    },
+                    
+                    .seek_element_name_end,
                     => unreachable,
                 },
                 
@@ -118,7 +192,10 @@ pub const Tokenizer = struct {
         tag_open,
         tag_element_open: enum {
             check_valid_start_char,
+            seek_element_name_end,
         },
+        
+        tag_pi_open,
     };
     
 };
@@ -126,7 +203,7 @@ pub const Tokenizer = struct {
 test "T1" {
     var tokenizer = Tokenizer{
         .buffer = 
-        \\<?xml version="1.0" encoding="UTF-8"?>
+        \\<faf version="1.0" encoding="UTF-8">
     };
     
     var current = tokenizer.next();
