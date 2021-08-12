f2ea674155ac8145ab1b4a769dcfc8ef34cdd2ac
InKryption <InKryption07@gmail.com>
2021-08-12 18:59:00 +0200

    Elegantly implemented element namespaces tokenization, and utf8 tracking conforming to the XML specs, put into a couple of validation functions

--- src/xml.zig
+++ src/xml.zig
@@ -22,7 +22,7 @@ pub const Tokenizer = struct {
             
             pub const Identifier = struct {
                 namespace: Range,
-                identifier: Range,
+                name: Range,
             };
             
             pub const Attribute = struct {
@@ -49,8 +49,15 @@ pub const Tokenizer = struct {
             instructions: Range,
         };
         
-        pub const Range = struct { beg: usize, end: usize };
-        pub const Index = struct { index: usize };
+        pub const Index = struct { index: usize, };
+        pub const Range = struct {
+            beg: usize,
+            end: usize,
+            
+            pub fn slice(self: Range, buffer: []const u8) []const u8 {
+                return buffer[self.beg..self.end];
+            }
+        };
     };
     
     pub fn next(self: *Tokenizer) Token {
@@ -108,7 +115,7 @@ pub const Tokenizer = struct {
                         self.parse_state = .{ .tag_element_open = .check_valid_start_char };
                         result = .{ .element_begin = .{ .identifier = .{
                             .namespace = .{ .beg = self.index, .end = self.index },
-                            .identifier = .{ .beg = self.index, .end = undefined, },
+                            .name = .{ .beg = self.index, .end = undefined, },
                         } } };
                     },
                     
@@ -118,66 +125,57 @@ pub const Tokenizer = struct {
                 => |*tag_element_open| switch (tag_element_open.*) {
                     .check_valid_start_char
                     => {
-                        switch (current_char) {
-                            'A'...'Z',
-                            'a'...'z',
-                            '\u{C0}'...'\u{D6}',
-                            '\u{D8}'...'\u{F6}',
-                            '_',
-                            => {
-                                tag_element_open.* = .seek_element_name_end;
-                                self.index += 1;
-                            },
-                            
-                            else
-                            => {
-                                const utf8_byte_seq_len = std.unicode.utf8ByteSequenceLength(current_char) catch {
-                                    if (true) unreachable;
-                                    result = .{ .invalid = .{ .index = self.index } };
-                                    break;
-                                };
-                                
-                                if (self.index + utf8_byte_seq_len > self.buffer.len) {
-                                    if (true) unreachable;
-                                    result = .{ .invalid = .{ .index = self.index } };
-                                    break;
-                                }
-                                
-                                const utf8_cp = std.unicode.utf8Decode(self.buffer[self.index..self.index + utf8_byte_seq_len]) catch {
-                                    if (true) unreachable;
-                                    result = .{ .invalid = .{ .index = self.index } };
-                                    break;
-                                };
-                                
-                                switch (utf8_cp) {
-                                    '\u{F8}'...'\u{2FF}',
-                                    '\u{370}'...'\u{37D}',
-                                    '\u{37F}'...'\u{1FFF}',
-                                    '\u{200C}'...'\u{200D}',
-                                    '\u{2070}'...'\u{218F}',
-                                    '\u{2C00}'...'\u{2FEF}',
-                                    '\u{3001}'...'\u{D7FF}',
-                                    '\u{F900}'...'\u{FDCF}',
-                                    '\u{FDF0}'...'\u{FFFD}',
-                                    '\u{10000}'...'\u{EFFFF}',
-                                    => {
-                                        tag_element_open.* = .seek_element_name_end;
-                                        self.index += utf8_byte_seq_len;
-                                    },
-                                    
-                                    else
-                                    => {
-                                        if (true) unreachable;
-                                        result = .{ .invalid = .{ .index = self.index } };
-                                        break;
-                                    },
-                                }
-                                
-                            },
+                        const utf8_cp = self.currentUtf8Codepoint() orelse {
+                            result = .{ .invalid = .{ .index = self.index } };
+                            break;
+                        };
+                        
+                        if (!isValidXmlNameStartCharUtf8(utf8_cp)) {
+                            result = .{ .invalid = .{ .index = self.index } };
+                            break;
                         }
+                        
+                        tag_element_open.* = .seek_element_name_end;
+                        self.index += std.unicode.utf8CodepointSequenceLength(utf8_cp) catch unreachable;
                     },
                     
                     .seek_element_name_end,
+                    => switch (current_char) {
+                        ' ', '\t', '\n', '\r',
+                        => {
+                            tag_element_open.* = .seek_tag_end;
+                            result.element_begin.identifier.name.end = self.index;
+                            break;
+                        },
+                        
+                        ':',
+                        => {
+                            result.element_begin.identifier.namespace.end = self.index;
+                            self.index += 1;
+                            result.element_begin.identifier.name.beg = self.index;
+                        },
+                        
+                        else
+                        => {
+                            
+                            const utf8_cp = self.currentUtf8Codepoint() orelse {
+                                result = .{ .invalid = .{ .index = self.index } };
+                                break;
+                            };
+                            
+                            if (!isValidXmlNameCharUtf8(utf8_cp)) {
+                                result = .{ .invalid = .{ .index = self.index } };
+                                break;
+                            }
+                            
+                            self.index += std.unicode.utf8ByteSequenceLength(current_char) catch {
+                                result = .{ .invalid = .{ .index = self.index } };
+                                break;
+                            };
+                        },
+                    },
+                    
+                    .seek_tag_end
                     => unreachable,
                 },
                 
@@ -187,12 +185,59 @@ pub const Tokenizer = struct {
         return result;
     }
     
+    fn currentUtf8Codepoint(self: Tokenizer) ?u21 {
+        const utf8_byte_seq_len = std.unicode.utf8ByteSequenceLength(self.buffer[self.index]) catch return null;
+        if (self.index + utf8_byte_seq_len > self.buffer.len) return null;
+        const utf8_cp = std.unicode.utf8Decode(self.buffer[self.index..self.index + utf8_byte_seq_len]) catch return null;
+        return utf8_cp;
+    }
+    
+    fn isValidXmlNameStartCharUtf8(char: u21) bool {
+        return switch (char) {
+            'A'...'Z',
+            'a'...'z',
+            '_',
+            '\u{c0}'    ... '\u{d6}',
+            '\u{d8}'    ... '\u{f6}',
+            '\u{f8}'    ... '\u{2ff}',
+            '\u{370}'   ... '\u{37d}',
+            '\u{37f}'   ... '\u{1fff}',
+            '\u{200c}'  ... '\u{200d}',
+            '\u{2070}'  ... '\u{218f}',
+            '\u{2c00}'  ... '\u{2fef}',
+            '\u{3001}'  ... '\u{d7ff}',
+            '\u{f900}'  ... '\u{fdcf}',
+            '\u{fdf0}'  ... '\u{fffd}',
+            '\u{10000}' ... '\u{effff}',
+            => true,
+            
+            else
+            => false,
+        };
+    }
+    
+    fn isValidXmlNameCharUtf8(char: u21) bool {
+        return isValidXmlNameStartCharUtf8(char) or switch (char) {
+            '0'...'9',
+            '-',
+            '.',
+            '\u{b7}',
+            '\u{0300}'...'\u{036f}',
+            '\u{203f}'...'\u{2040}',
+            => true,
+            
+            else
+            => false,
+        };
+    }
+    
     pub const ParseState = union(enum) {
         start,
         tag_open,
         tag_element_open: enum {
             check_valid_start_char,
             seek_element_name_end,
+            seek_tag_end,
         },
         
         tag_pi_open,
@@ -203,9 +248,14 @@ pub const Tokenizer = struct {
 test "T1" {
     var tokenizer = Tokenizer{
         .buffer = 
-        \\<faf version="1.0" encoding="UTF-8">
+        \\<dir:ñañ dof="1.0"
+        \\  tin="UTF-8"
+        \\>
     };
     
     var current = tokenizer.next();
-    _ = current;
+    std.debug.print("\n{s}:", .{current.element_begin.identifier.namespace.slice(tokenizer.buffer)});
+    std.debug.print("{s}\n", .{current.element_begin.identifier.name.slice(tokenizer.buffer)});
+    
+    //current = tokenizer.next();
 }
