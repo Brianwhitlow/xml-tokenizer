c1767b409f96558f6a9a4c3f708f32c7d630a2b3
InKryption <InKryption07@gmail.com>
2021-08-13 04:54:34 +0200

    namely, these functions

--- src/xml_messy.zig
+++ src/xml_messy.zig
@@ -188,45 +188,6 @@ pub const Tokenizer = struct {
         return utf8_cp;
     }
     
-    fn isValidXmlNameStartCharUtf8(char: u21) bool {
-        return switch (char) {
-            'A'...'Z',
-            'a'...'z',
-            '_',
-            '\u{c0}'    ... '\u{d6}',
-            '\u{d8}'    ... '\u{f6}',
-            '\u{f8}'    ... '\u{2ff}',
-            '\u{370}'   ... '\u{37d}',
-            '\u{37f}'   ... '\u{1fff}',
-            '\u{200c}'  ... '\u{200d}',
-            '\u{2070}'  ... '\u{218f}',
-            '\u{2c00}'  ... '\u{2fef}',
-            '\u{3001}'  ... '\u{d7ff}',
-            '\u{f900}'  ... '\u{fdcf}',
-            '\u{fdf0}'  ... '\u{fffd}',
-            '\u{10000}' ... '\u{effff}',
-            => true,
-            
-            else
-            => false,
-        };
-    }
-    
-    fn isValidXmlNameCharUtf8(char: u21) bool {
-        return isValidXmlNameStartCharUtf8(char) or switch (char) {
-            '0'...'9',
-            '-',
-            '.',
-            '\u{b7}',
-            '\u{0300}'...'\u{036f}',
-            '\u{203f}'...'\u{2040}',
-            => true,
-            
-            else
-            => false,
-        };
-    }
-    
     pub const ParseState = union(enum) {
         start,
         tag_open,
