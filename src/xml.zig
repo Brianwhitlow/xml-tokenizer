ed2072338d3725423ad46f99ab6e29dd046214c4
InKryption <InKryption07@gmail.com>
2021-09-13 03:42:39 +0200

    Did some reorganizing

--- src/xml.zig
+++ src/xml.zig
@@ -1,1 +1,53 @@
-pub usingnamespace @import("token_stream.zig");
+const std = @import("std");
+const testing = std.testing;
+
+pub const TokenStream = @import("xml/TokenStream.zig");
+
+const token = @import("xml/token.zig");
+pub const Token = token.Token;
+pub const Range = token.Range;
+pub const Index = token.Index;
+
+pub fn isValidNameStartCharUtf8(char: u21) bool {
+    return switch (char) {
+        'A'...'Z',
+        'a'...'z',
+        '_',
+        '\u{c0}'    ... '\u{d6}',
+        '\u{d8}'    ... '\u{f6}',
+        '\u{f8}'    ... '\u{2ff}',
+        '\u{370}'   ... '\u{37d}',
+        '\u{37f}'   ... '\u{1fff}',
+        '\u{200c}'  ... '\u{200d}',
+        '\u{2070}'  ... '\u{218f}',
+        '\u{2c00}'  ... '\u{2fef}',
+        '\u{3001}'  ... '\u{d7ff}',
+        '\u{f900}'  ... '\u{fdcf}',
+        '\u{fdf0}'  ... '\u{fffd}',
+        '\u{10000}' ... '\u{effff}',
+        => true,
+        
+        else
+        => false,
+    };
+}
+
+pub fn isValidNameCharUtf8(char: u21) bool {
+    return isValidNameStartCharUtf8(char) or switch (char) {
+        '0'...'9',
+        '-',
+        '.',
+        '\u{b7}',
+        '\u{0300}'...'\u{036f}',
+        '\u{203f}'...'\u{2040}',
+        => true,
+        
+        else
+        => false,
+    };
+}
+
+comptime {
+    _ = token;
+    _ = TokenStream;
+}
